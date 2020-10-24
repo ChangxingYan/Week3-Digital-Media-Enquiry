f3264cebbae3950262e57fa5fa2fc37587d812d0
ChangxingYan <dtnvcy2@ucl.ac.uk>
2020-10-24 20:03:17 +0100

    task accomplished
    1 is in GameController.cs
    2-5 are in obstaclesGenerator

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+//using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class GameController : MonoBehaviour
@@ -7,12 +10,22 @@ public class GameController : MonoBehaviour
 
     public GameObject myObstaclePrefab;
 
-    
+    float range = 3f;
+    int interactor = 10;
 
     // Start is called before the first frame update
     void Start()
     {
+        GameObject sphere1;
         
+
+        int counter = 0;
+    while (counter < interactor)
+        {
+            sphere1 = Instantiate(myObstaclePrefab);
+            sphere1.transform.position = new Vector3(UnityEngine.Random.Range(-range, range), UnityEngine.Random.Range(0, range), UnityEngine.Random.Range(-range, range));
+            counter++;
+        }
     }
 
     // Update is called once per frame
@@ -20,4 +33,9 @@ public class GameController : MonoBehaviour
     {
         
     }
+
+  
+
 }
+
+
---
+++ Assets/Scripts/obstaclesGenerator.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class obstaclesGenerator : MonoBehaviour
+{
+
+    public GameObject myObstaclePrefab; //connect with sphere 'OB'
+    public GameObject Generator; //just for distingushing old & new,connect with square 'obstacle'
+    float newRange = 1f;
+    int interactor = 10;
+    int counter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+
+      
+    
+     
+
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+    
+    void OnCollisionEnter(Collision collision)
+    {
+    
+        if (collision.gameObject.tag == "Player")
+        {   //1 is in GameCotroller.cs
+
+            //2
+            print("Boom!");
+
+            //3
+            GameObject sphere2;
+            Destroy(gameObject);  
+
+            //4
+            sphere2 = Instantiate(Generator);
+            sphere2.transform.position = gameObject.transform.position + new Vector3(UnityEngine.Random.Range(0, newRange), UnityEngine.Random.Range(0, newRange), UnityEngine.Random.Range(-newRange, newRange));
+
+            sphere2 = Instantiate(Generator);
+            sphere2.transform.position = gameObject.transform.position + new Vector3(UnityEngine.Random.Range(-newRange,0), UnityEngine.Random.Range(0, newRange), UnityEngine.Random.Range(-newRange, newRange));
+
+        }//linked to obstacle
+
+
+
+        //5
+        else if (collision.gameObject.tag == "Desk")
+        {
+            
+
+            GameObject[] balls = GameObject.FindGameObjectsWithTag("Obstacle");
+            counter = interactor ;
+            foreach (GameObject Obstacle in balls)
+            {
+                if (GetComponent<Renderer>().isVisible)
+                {
+                    counter--;
+                }
+            }
+         
+
+            int left = interactor - counter;
+            print("You Win!");
+            print("Obstacles left:" + left);
+            print("You hit:" + counter);
+
+        }//linked to FakeGreg
+       
+    }
+}
